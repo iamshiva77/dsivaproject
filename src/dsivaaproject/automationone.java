56ba6d7821e1c30050a1211cbd9a1ec22f4aaa5c
iamshiva77 <sivaraam.d@gmail.com>
2024-05-08 19:30:57 +0530

    Update automationone.java

--- src/dsivaaproject/automationone.java
+++ src/dsivaaproject/automationone.java
@@ -27,5 +27,5 @@ public class automationone extends sivaone {
 		elementtobeclickble(Duration.ofSeconds(20),Username );
 		Username.sendKeys("adminuser@socrates");
 	}
-	System.out.println("fork is added to tthhhtht the github");
+	System.out.println("fork is added to the github");
 }
